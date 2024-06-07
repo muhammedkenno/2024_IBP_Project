7dfc6b18f39a1ebd35f8f24617d8cbb4953073f9
muhammedkenno <yoskno1@gmail.com>
2024-06-08 02:04:26 +0300

    first commit

---
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://wallpaperswide.com/download/3d_yellow_spiral-wallpaper-1920x600.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Best Collection<br /> EVREY TCHNO
+        </h1>
+        <p className="pt-5 text-[16] font-[Poppins] font-[400] text-[rgb(255,255,255)]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
+          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
+          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
+          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} mt-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/server.js
@@ -0,0 +1,2 @@
+// export const server = "https://eshop-tutorial.vercel.app/api/v2";
+export const server = "http://localhost:9000/api/v2";
